3158965bb2f43ad6b7d7497fcf35d5aa2b2416b3
HaLan0101 <dnhalan0101@gmail.com>
2023-04-22 12:59:46 +0200

    fix

--- ChatWithGPT/src/components/LoginForm/LoginForm.js
+++ ChatWithGPT/src/components/LoginForm/LoginForm.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
+import { PermissionsAndroid } from 'react-native';
 import { useTranslation } from 'react-i18next';
+import { Share } from 'react-native';
+import notifee from '@notifee/react-native';
 const LoginForm = ({ handleLogin }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -8,7 +11,77 @@ const LoginForm = ({ handleLogin }) => {
   const handlePress = () => {
     handleLogin(email, password);
   };
+  useEffect(() => {
+    requestPermission();
+  }, []);
 
+  const requestPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      ]);
+      if (
+        granted[PermissionsAndroid.PERMISSIONS.CAMERA] ===
+        PermissionsAndroid.RESULTS.GRANTED &&
+        granted[PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE] ===
+        PermissionsAndroid.RESULTS.GRANTED &&
+        granted[PermissionsAndroid.PERMISSIONS.RECORD_AUDIO] ===
+        PermissionsAndroid.RESULTS.GRANTED
+      ) {
+        console.log('You can use the camera, gallery, and microphone');
+      } else {
+        console.log('Camera, gallery, or microphone permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const handleSharePress = async () => {
+    try {
+      const result = await Share.share({
+        message: 'Check out this ChatWithGtp: https://example.com',
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          console.log('Shared with activity type:', result.activityType);
+        } else {
+          console.log('Shared');
+        }
+      } else if (result.action === Share.dismissedAction) {
+        console.log('Dismissed');
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  async function onDisplayNotification() {
+    await notifee.requestPermission()
+
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    await notifee.displayNotification({
+      title: 'Défis tes amis !',
+      body: 'Tu aime la musique ? Alors tu viens jouer avec tes maintenant !',
+      android: {
+        channelId,
+        smallIcon: 'ic_launcher',
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
+
+  notifee.onBackgroundEvent(async ({ type, detail }) => {
+    if (type === 'notification-action-press') {
+    }
+  });
   return (
     <Container>
       <Title>{t('titleConnexion')}</Title>
@@ -27,6 +100,13 @@ const LoginForm = ({ handleLogin }) => {
       <Button onPress={handlePress}>
         <ButtonText>{t('buttonLogin')}</ButtonText>
       </Button>
+      <ShareButton onPress={handleSharePress}>
+        <ShareIcon source={require('../../icones/share.png')} />
+        <ShareButtonText>Share</ShareButtonText>
+      </ShareButton>
+      <NotificationButton onPress={() => onDisplayNotification()}>
+        <NotificationButtonText>{t('notification')}</NotificationButtonText>
+      </NotificationButton>
     </Container>
   );
 };
@@ -67,6 +147,36 @@ const Button = styled.TouchableOpacity`
 
 const ButtonText = styled.Text`
   color: ${props => props.theme.textColor};
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const ShareButton = styled.TouchableOpacity`
+  background-color:  ${props => props.theme.secondaryColor};
+  padding: 10px;
+  border-radius: 5px;
+  margin-top: 20px;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const ShareButtonText = styled.Text`
+  color: #FFFFFF;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const ShareIcon = styled.Image`
+  width: 25px;
+  height: 25px;
+`;
+
+const NotificationButton = styled.TouchableOpacity`
+  margin-top: 20px;
+`;
+
+const NotificationButtonText = styled.Text`
+  color: ${props => props.theme.secondaryColor};
   font-size: 16px;
   font-weight: bold;
 `;
